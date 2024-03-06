b34ff7f38ba87976a0555f609478363ecd2b0f50
seville <amirrezasalimi0@gmail.com>
2024-03-06 15:39:04 +0330

    chore: fix host url in script embed

--- frontend/src/shared/utils/get-code.ts
+++ frontend/src/shared/utils/get-code.ts
@@ -1,11 +1,10 @@
 export default function getScriptCode(id: string) {
-    const host = process.env.NODE_ENV === 'production' ? 'https://gl.toolstack.run' : 'http://localhost:3000'
-    return `<script src="http://localhost:3000/script.js" defer></script>
+  return `<script src="${process.env.NEXT_PUBLIC_HOST_URL}script.js" defer></script>
 <script>
     document.addEventListener("DOMContentLoaded", function () {
         if (typeof window.golingo != "undefined") {
             golingo.init({ code: '${id}' })
         }
     })
-</script>`
+</script>`;
 }
